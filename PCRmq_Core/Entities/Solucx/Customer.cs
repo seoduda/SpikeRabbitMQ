46b022b306e29becc57441340542fbfc6968c203
eduardo furtado <eduardo.furtado@loccitane.com>
2024-04-03 18:55:24 -0300

    Sender 0.8

---
+++ PCRmq_Core/Entities/Solucx/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace PCRmq_Core.Entities.Solucx
+{
+    public class Customer
+    {
+        public string id { get; set; }
+        public string client_id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public string phone2 { get; set; }
+        public string cpf { get; set; }
+        public object gender { get; set; }
+        public bool opt_out { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
--- Producer/Sender.cs
+++ Producer/Sender.cs
@@ -1,10 +1,50 @@
-﻿namespace Producer
+﻿using PCRmq_Core.Entities.Solucx;
+using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+namespace Producer
 {
     internal class Sender
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.UserName = "duda";
+            factory.Password = "panama";
+            factory.HostName = "192.168.99.66";
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+                string message = GetPayload();
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish("", "BasicTest", null, body);
+                Console.WriteLine("Sent {0} ", message);
+            }
+
+            Console.WriteLine("Press anu key to close ");
+            Console.ReadLine();
+        }
+
+        private static string GetPayload()
+        {
+            Customer customer = new Customer
+            {
+                id = "41",
+                client_id = "24944697830",
+                name = "EDUARDO TEIXEIRA FURTADO",
+                email = "eduardo.furtado@gmail.com",
+                phone = "11976745086",
+                phone2 = "",
+                cpf = "24944697830",
+                gender = null,
+                opt_out = false,
+                CreationDate = DateTime.Now
+            };
+
+            return JsonSerializer.Serialize(customer);
+
         }
     }
 }
